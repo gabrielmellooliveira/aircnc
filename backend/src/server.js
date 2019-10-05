c55ad6b877df280e39025d438912c80abba12418
gabrielmellooliveira <gabrielmello3011@gmail.com>
2019-10-04 22:50:34 -0300

    Add backend project

---
+++ backend/src/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const socketio = require('socket.io');
+const http = require('http');
+
+const routes = require('./routes');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+mongoose.connect('mongodb+srv://aircnc:gabrielmellooliveira@aircnc-ftmdf.mongodb.net/aircnc?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Seria melhor utilizar o Redis para salvar essas informações, pois toda vez que o servidor reiniciar iriamos perder essas informações
+const connectedUsers = {};
+
+io.on('connect', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+server.listen(3333);
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
