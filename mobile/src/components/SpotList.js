06eb69fcd1fa05fbb8509d9c9efdc0bf7a66d493
gabrielmellooliveira <gabrielmello3011@gmail.com>
2019-10-05 17:22:03 -0300

    Add Spot component

--- mobile/src/components/SpotList.js
+++ mobile/src/components/SpotList.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { withNavigation } from 'react-navigation';
-import { View, Text, FlatList, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 import api from '../services/api';
+import Spot from './Spot';
 
-function SpotList({ tech, navigation }) {
+export default function SpotList({ tech }) {
     const [spots, setSpots] = useState([]);
 
     useEffect(() => {
@@ -19,10 +19,6 @@ function SpotList({ tech, navigation }) {
         loadSpots();
     }, []);
 
-    function handleNavigate(id) {
-        navigation.navigate('Book', { id });
-    }
-
     return (
         <View style={styles.container}>
             <Text style={styles.title}>
@@ -35,16 +31,7 @@ function SpotList({ tech, navigation }) {
                 keyExtractor={spot => spot._id}
                 horizontal
                 showsHorizontalScrollIndicator={false}
-                renderItem={({ item }) => (
-                    <View style={styles.listItem}>
-                        <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url }} />
-                        <Text style={styles.company}>{item.company}</Text>
-                        <Text style={styles.price}>{item.price ? `R$${item.price}/dia` : 'GRATUITO'}</Text>
-                        <TouchableOpacity onPress={_ => handleNavigate(item._id)} style={styles.button}>
-                            <Text style={styles.buttonText}>Solicitar reserva</Text>
-                        </TouchableOpacity>
-                    </View>
-                )}
+                renderItem={({ item }) => <Spot item={item} />}
             />
         </View>
     );
@@ -65,40 +52,5 @@ const styles = StyleSheet.create({
     },
     list: {
         paddingHorizontal: 20
-    },
-    listItem: {
-        marginRight: 15
-    },
-    thumbnail: {
-        width: 200,
-        height: 120,
-        resizeMode: 'cover',
-        borderRadius: 2
-    },
-    company: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#333',
-        marginTop: 10
-    },
-    price: {
-        fontSize: 15,
-        color: '#999',
-        marginTop: 5
-    },
-    button: {
-        height: 32,
-        backgroundColor: '#F05A5B',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 2,
-        marginTop: 15
-    },
-    buttonText: {
-        color: '#FFF',
-        fontWeight: 'bold',
-        fontSize: 15
     }
 });
-
-export default withNavigation(SpotList);
